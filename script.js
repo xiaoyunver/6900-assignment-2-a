4d61fc227c032681da34af4d26d28736c157e55e
xiaoyunver <xinhe.yu@husky.neu.edu>
2016-03-12 22:17:34 -0500

    some changes

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function dataLoaded(err,rows){
 
     //Total number of trips in 2012
     //filter by start time
-    tripsByTime.filter([new Date(2012,1,1,0,0,0),new Date(2012,12,31,11,59,59)]);
+    tripsByTime.filter([new Date(2012,0,1,0,0,0),new Date(2012,11,31,11,59,59)]);//(2012,0,1),(2012,11,31)
     console.log('Total number of trips in 2012: '+tripsByTime.top(Infinity).length);
 
     //Total number of trips in 2012 AND taken by male, registered users
@@ -39,7 +39,7 @@ function dataLoaded(err,rows){
 
     //Top 50 trips, in all time, by all users, regardless of starting point, in terms of trip duration.
     //Reset Start Station filter
-    tripsBySStation.filter(null);
+    tripsBySStation.filter(null);//tripsBySStation.filterAll()
     tripsByTime.filter(null);
 
     //console log
@@ -52,7 +52,7 @@ function dataLoaded(err,rows){
 
     //Group all trips into 10-year age buckets
 
-    var tripsByAgeGroup = tripsByAge.group(function(d){return Math.floor(d/10);})
+    var tripsByAgeGroup = tripsByAge.group(function(d){return Math.floor(d/10);});
     console.log('Grouped all trips into 10-year age: ');
     console.log(tripsByAgeGroup.all());
 
@@ -71,7 +71,7 @@ function parse(d){
         endStation: d.end_statn,
         gender: d.gender==''? '0': d.gender,
         type: d.subsc_type,
-        age: ageFunction(+d.birth_date)
+        age: ageFunction(+d.birth_date,parseDate(d.start_date))
     }
 }
 
@@ -82,14 +82,20 @@ function parseDate(date){
     return new Date(+day[2],+day[0]-1, +day[1], +time[0], +time[1]);
 }
 
-function ageFunction(birthDate)
+function ageFunction(birthDate,startDate)
 {
-    var currentDate= new Date();
-    var currentYear= currentDate.getFullYear();
+    // set current year;
+    //var currentDate= new Date();//changing to using start date (start_date)
+    //var currentYear= currentDate.getFullYear();
+
+    //set year of start_date
+    var startYear = startDate.getFullYear();
+    //console.log('1:'+startYear);
+    //console.log('2:'+startDate);
 
     if(birthDate=='')
     return 'one';
     else
-    return (currentYear-birthDate);
+    return (startYear-birthDate);
 }
 
